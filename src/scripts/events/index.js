3636f2f7ce9300d79e1ab0ef69b7b329e0f2d7a3
Burak ŞEN <buraksenb@gmail.com>
2021-12-19 00:37:56 +0300

    Add email sender to the original gelsin app.

---
+++ src/scripts/events/index.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer");
+const eventEmitter = require("./eventEmitter");
+
+module.exports = () => {
+  eventEmitter.on("send_email", (emailData) => {
+    let transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+    const info = transporter.sendMail({
+      from: process.env.EMAIL_FROM,
+      ...emailData,
+    });
+  });
+};
